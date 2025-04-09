2f1c223e7ef8d1e05d0d32a1892a63a72271de2f
Rain0829 <rain08290706@gmail.com>
2025-04-09 16:57:11 -0400

    update the readFileToList

--- src/SurveyDataAnalyzer.java
+++ src/SurveyDataAnalyzer.java
@@ -1,4 +1,33 @@
+import javax.sound.sampled.Line;
+import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+
 public class SurveyDataAnalyzer {
     public static CustomHashTable readResponsesFromFile(String filePath){
+        List<String> data = readFileToList(filePath);
+        CustomHashTable customHashTable = new CustomHashTable();
     }
-}
+
+
+
+    public static List<String> readFileToList(String filePath){
+        List<String> lines = new ArrayList<>();
+        try {
+            lines = Files.readAllLines(Paths.get(filePath));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    public static void main(String[] args) {
+        List<String> list = readFileToList("responses.txt");
+        System.out.println(list);
+    }
+
+    }
