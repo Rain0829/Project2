9230be309bf3a7433c3734cfb43ca1a434ceaab4
Rain0829 <rain08290706@gmail.com>
2025-04-09 10:46:10 -0400

    update the CustomHashTable.java update the smoker binary choice

---
+++ src/TestResponse.java
@@ -0,0 +1,13 @@
+public class TestResponse {
+
+    public static void main(String[] args) {
+        Response response = new Response(1, "M", 3, 1, 3, 2, 1, 1, 2022, "Some string", 1, 2, 3, 4, 1, 2, "Another string", 2, 3, 4, 1, 2,  "Yet another string",3, 3, 4, 1, 2, 3, 4, 5);
+
+        // Display information from the Response object
+        System.out.println("ID: " + response.getId());
+        System.out.println("Gender: " + response.getGender());
+        System.out.println("Age: " + response.getAge());
+        System.out.println("Quality: " + response.getQuality());
+        response.printResponse();
+    }
+}
